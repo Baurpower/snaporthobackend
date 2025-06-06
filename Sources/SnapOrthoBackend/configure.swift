3f9dd99a9285e67822228e334d6b70cd7788ee78
Baurpower <alexbaur123@gmail.com>
2025-06-06 08:35:07 -0400

    error

--- Sources/SnapOrthoBackend/configure.swift
+++ Sources/SnapOrthoBackend/configure.swift
@@ -3,22 +3,12 @@ import Fluent
 import FluentPostgresDriver
 import Vapor
 
-// configures your application
 public func configure(_ app: Application) async throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
-        tls: .prefer(try .init(configuration: .clientDefault)))
-    ), as: .psql)
 
+    // Add migrations (e.g., CreateTodo)
     app.migrations.add(CreateTodo())
 
-    // register routes
+    // ✅ Register routes
     try routes(app)
 }
