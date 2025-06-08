8513813148f48429adedf239bf463c35fb9ea99f
Baurpower <alexbaur123@gmail.com>
2025-06-08 12:55:15 -0400

    d

--- Package.swift
+++ Package.swift
@@ -34,6 +34,9 @@ let package = Package(
 
 
             ],
+            resources: [
+                            .copy("Public")
+                        ],
             swiftSettings: swiftSettings
         ),
         .testTarget(
